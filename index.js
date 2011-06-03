73f51f9bf8b87427e8b85d2d79e72a34ab37cf0b
André Milton <andre@mediafresh.net>
2011-06-03 14:22:45 -0400

    still figuring it out

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var app = require('express').createServer();
+var express = require('express');
+var app = express.createServer(
+	express.cookieParser()
+);
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.get('/', function(req, res){
-  res.send('hello world');
+	res.send('Awesome...');
 });
 
 app.listen(3000);
