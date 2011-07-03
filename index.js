cbd986d6020c6e1ad7093be275339d4003a84a14
Wred <andre@mediafresh.net>
2011-07-03 17:29:04 -0400

    added mysql connection and users

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var login = require('./login');
+var login = require('./login.js');
 var app = express.createServer();
-//var RedisStore = require('connect-redis')(express);
+var RedisStore = require('connect-redis')(express);
 
 app.configure(function () {
 	app.set('view engine', 'jade');
 	app.set('view options', {layout:false});
 	app.use(express.favicon());
 	app.use(express.cookieParser());
-	app.use(express.session({secret:"s3cr3t"/*, store:new RedisStore */}));
+	app.use(express.session({secret:"s3cr3t", store:new RedisStore}));
 	app.use(express.bodyParser());
 	app.use(express.compiler({ src: __dirname + '/views/style', enable: ['less']}));
 	app.use(express.query());
@@ -20,7 +20,13 @@ app.configure('development',function(){
 });
 
 app.get('/', function(req, res){
-	res.send('Logged into Fresher...');
+	res.render("main");
+});
+
+app.get('/logout', function(req, res) {
+	req.session.token = null;
+	res.writeHead(302, {'Location': '/'});
+	res.end();
 });
 
 app.listen(3000);
--- login.js
+++ login.js
@@ -1,18 +1,40 @@
+var MysqlClient = require("mysql").Client, db = new MysqlClient();
+//var util = require("util");
+
+db.user = "root";
+db.password = "mysqlr00t";
+
 exports = module.exports = function login() {
 	return function login(req, res, next) {
 		if (req.session.token != null) {
 			next();
-		}
-		else if (req.query.user) {
-			if ((req.query.user == "test") && (req.query.password == "test")) {
-				req.session.token = "working!"
-				next();
-			}
-			else {
-				res.render("login",
-					{error:{id:1,msg:"Invalid username or password"},
-					query:req.query});
-			}
+		} 
+		else if (req.query.project) {
+			db.database = req.query.project;
+			
+			db.connect(function (err) {
+				if (err) {
+					res.render("login",
+						{error:{id:1,msg:"Invalid project"},
+						query:req.query});
+				} else {
+					db.query("SELECT ID FROM Users WHERE"+
+						" username = '"+ req.query.user +"'"+
+						" AND password = '" + req.query.password +"'",
+						function (err, results, fields) {
+							if (results.length) {
+								req.session.token = "working!"
+								next();
+							}
+							else {
+								res.render("login",
+									{error:{id:3,msg:"Invalid username or password"},
+									query:req.query});
+							}
+						}
+					);
+				}
+			})
 		}
 		else {
 			res.render("login",
